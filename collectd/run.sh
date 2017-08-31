56d6e278da8510171bc08abe1d3c5a5ae37538db
Mehdi Abaakouk <sileht@sileht.net>
2017-08-31 16:44:03 +0200

    Use rate for network interface

--- collectd/run.sh
+++ collectd/run.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-set -e
 exec >&2
 
 export COLLECTD_HOST=fake-phy-host-$(hostname -f)
+export OS_AUTH_TYPE=gnocchi-basic
+export GNOCCHI_USER=admin
+export GNOCCHI_ENDPOINT=http://gnocchi-api:8041/
 
+while [[ $( curl -Lk --noproxy '*' -s -o /dev/null -w '%{http_code}' $GNOCCHI_ENDPOINT ) != 200 || $? -eq 7 ]]; do
+    sleep 1
+done
+
+configured=$(gnocchi archive-policy-rule list -c name -f value | grep network-interface-rate)
+if [ ! "$configured" ] ; then
+    gnocchi archive-policy create -d granularity:0:05:00,points:8640 rate:low -m rate:last
+    gnocchi archive-policy-rule create -m "interface-*@if_*" -a rate:low network-interface-rate
+fi
+
+set -e
 echo "collectd host: $COLLECTD_HOST"
 envtpl --keep-template /etc/collectd/collectd.conf.tpl
 
