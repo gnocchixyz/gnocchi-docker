1936cc691753a959473452082bcd8c1e2bfea8fa
Mehdi Abaakouk <sileht@sileht.net>
2017-12-07 12:52:34 +0100

    collectd: stop generate load
    Now, the default dashboard have network/disk/...
    
    No need to generate cpu load to have graph.

--- collectd/run.sh
+++ collectd/run.sh
@@ -24,10 +24,4 @@ echo "collectd host: $COLLECTD_HOST"
 envtpl --keep-template /etc/collectd/collectd.conf.tpl
 
 echo "starting collectd"
-collectd -f &
-
-echo "starting stress"
-while true ; do
-    stress -c 1 -m 2 -i 1 -t 5 --vm-hang 2
-    sleep 5
-done
+collectd -f
